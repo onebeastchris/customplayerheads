ae5a372b9c96c1fcd8e5da04083e0839ad5350bc
onebeastchris <reichel32574@gmail.com>
2022-09-06 15:46:00 +0200

    check for . in playername

--- src/main/java/net/onebeastofchris/geyserplayerheads/mixin/LivingEntityMixin.java
+++ src/main/java/net/onebeastofchris/geyserplayerheads/mixin/LivingEntityMixin.java
@@ -4,8 +4,11 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
+import net.onebeastofchris.geyserplayerheads.GeyserPlayerHeads;
 import org.spongepowered.asm.mixin.Mixin;
 // import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,24 +22,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+
+	@Inject(method = "dropLoot", at = @At("TAIL"))
+	private void gph$dropHead(DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
+		if (this.livingEntity instanceof PlayerEntity player) {
+			String playerName = String.valueOf(player.getEntityName());
+			String playerDeathPos = String.valueOf(player.getBlockPos());
+			//dropItem(Items.PLAYER_HEAD);
+			GeyserPlayerHeads.LOGGER.info(playerName + " died at " + playerDeathPos);
+
+			if (playerName.contains("."))
+			{
+
+			} else dropItem(Items.PLAYER_HEAD.getDefaultStack().setCustomName(Text.of("Steve")).getItem());
+		}
+	}
 //	@Shadow protected abstract void dropInventory();
 
 //	@Shadow protected abstract void drop(DamageSource source);
 
-//	private final LivingEntity livingEntity = (LivingEntity) (Object) this;
-//	private final LivingEntity attacker = livingEntity.getAttacker();
+	private final LivingEntity livingEntity = (LivingEntity) (Object) this;
+	private final LivingEntity attacker = livingEntity.getAttacker();
 
 	public LivingEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
-	}
-
-	private void dropHead() {
-		dropItem(Items.PLAYER_HEAD);
-	}
-
-	@Inject(method = "dropLoot", at = @At("TAIL"))
-	private void gph$dropHead(DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
-		dropHead();
 	}
 
 }

c1a70b0dfa47ebad5b7fd9c27d4c63b38292ae0c
Jens <jenscollaertprive@hotmail.com>
2022-09-12 00:59:03 +0200

    Enhance server query and response.

--- src/main/java/net/onebeastofchris/geyserplayerheads/GeyserPlayerHeads.java
+++ src/main/java/net/onebeastofchris/geyserplayerheads/GeyserPlayerHeads.java
@@ -1,6 +1,10 @@
 package net.onebeastofchris.geyserplayerheads;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.world.ServerWorld;
+import net.onebeastofchris.geyserplayerheads.events.PlayerJoinEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,8 +19,13 @@ public class GeyserPlayerHeads implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		registerEvents();
 		logger = LoggerFactory.getLogger("gph");
 		logger.info("GeyserPlayerHeads starting now");
+	}
+
+	private void registerEvents() {
+		ServerEntityEvents.ENTITY_LOAD.register((Entity entity, ServerWorld world) -> PlayerJoinEvent.onSpawn(world, entity));
 	}
 
 	public static Logger getLogger() {

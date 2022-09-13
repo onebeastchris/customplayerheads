cb9ddc4c6b434cc0e7971208624eddc40c9ca579
onebeastchris <reichel32574@gmail.com>
2022-09-13 17:50:13 +0200

    *getAttacker reworked to source.getAttacker()

--- src/main/java/net/onebeastofchris/geyserplayerheads/mixin/LivingEntityMixin.java
+++ src/main/java/net/onebeastofchris/geyserplayerheads/mixin/LivingEntityMixin.java
@@ -18,37 +18,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 /**
  * Credit for parts of the following  code to DakotaPride
  * <a href="https://github.com/DakotaPride/Incantation-Fabric/blob/master/src/main/java/net/dakotapride/incantation/mixin/LivingEntityMixin.java">...</a>
- * **/
+ **/
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
 
-	@Inject(method = "dropLoot", at = @At("TAIL"))
-	private void gph$dropHead(DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
-		if (this.livingEntity instanceof PlayerEntity player) {
-			GeyserPlayerHeads.getLogger().info(player.getEntityName() + " died at " + player.getBlockPos().toString());
+    @Inject(method = "dropLoot", at = @At("TAIL"))
+    private void gph$dropHead(DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
+        if (this.livingEntity instanceof PlayerEntity player) {
+            GeyserPlayerHeads.getLogger().info(player.getEntityName() + " died at " + player.getBlockPos().toString());
+            var head = Items.PLAYER_HEAD.getDefaultStack();
+            if (player.getEntityName().startsWith(".") || FloodgateUser.isFloodgatePlayer(player.getUuid())) {
+                head.setNbt(PlayerJoinEvent.getTextureID().get(player.getUuid()).getBedrockNbt(source.getAttacker()));
+            } else {
+                head.setNbt(PlayerJoinEvent.getTextureID().get(player.getUuid()).getJavaNbt(source.getAttacker()));
+            }
+            dropStack(head);
+        }
+    }
 
-			var head = Items.PLAYER_HEAD.getDefaultStack();
-			if (player.getEntityName().startsWith(".") || FloodgateUser.isFloodgatePlayer(player.getUuid())) {
-				head.setNbt(PlayerJoinEvent.getTextureID().get(player.getUuid()).getBedrockNbt());
-			} else {
-				head.setNbt(PlayerJoinEvent.getTextureID().get(player.getUuid()).getJavaNbt());
-			}
-			dropStack(head);
-		}
-	}
+    private final LivingEntity livingEntity = (LivingEntity) (Object) this;
 
-	private String gph$getAttacker(LivingEntity pEntity){
-		if (pEntity instanceof PlayerEntity player){
-			return player.getEntityName();
-		} else return null;
-	}
-
-//	@Shadow protected abstract void drop(DamageSource source);
-	private final LivingEntity livingEntity = (LivingEntity) (Object) this;
-	private final LivingEntity attacker = livingEntity.getAttacker();
-	public LivingEntityMixin(EntityType<?> type, World world) {
-		super(type, world);
-	}
-
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
 }

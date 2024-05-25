33116047b3498405676024ea5a65946dc8decf4f
chris <github@onechris.mozmail.com>
2024-05-25 23:42:01 +0200

    Dont create new thread to drop a head

--- src/main/java/net/onebeastofchris/customplayerheads/mixin/LivingEntityMixin.java
+++ src/main/java/net/onebeastofchris/customplayerheads/mixin/LivingEntityMixin.java
@@ -34,26 +34,22 @@ public abstract class LivingEntityMixin extends Entity {
     @Inject(method = "dropLoot", at = @At("TAIL"))
     private void gph$dropHead(DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
         if (this.livingEntity instanceof ServerPlayerEntity player && CustomPlayerHeads.config.isShouldDropHeadsOnDeath()) {
-            try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
-                executor.execute(() -> {
-                    ItemStack skull = Items.PLAYER_HEAD.getDefaultStack();
+            ItemStack skull = Items.PLAYER_HEAD.getDefaultStack();
 
-                    skull.set(DataComponentTypes.PROFILE, new ProfileComponent(player.getGameProfile()));
-                    skull.set(DataComponentTypes.CUSTOM_NAME, TextureUtils.customNameComponent(player.getNameForScoreboard()));
+            skull.set(DataComponentTypes.PROFILE, new ProfileComponent(player.getGameProfile()));
+            skull.set(DataComponentTypes.CUSTOM_NAME, TextureUtils.customNameComponent(player.getNameForScoreboard()));
 
-                    if (source.getAttacker() instanceof ServerPlayerEntity killer) {
-                        if (CustomPlayerHeads.config.isShowLore()) {
-                            skull.set(DataComponentTypes.LORE, TextureUtils.getLoreComponent(killer));
-                        }
-                        this.dropStack(skull);
-                        return;
-                    }
+            if (source.getAttacker() instanceof ServerPlayerEntity killer) {
+                if (CustomPlayerHeads.config.isShowLore()) {
+                    skull.set(DataComponentTypes.LORE, TextureUtils.getLoreComponent(killer));
+                }
+                this.dropStack(skull);
+                return;
+            }
 
-                    // case: player killed by non-player
-                    if (CustomPlayerHeads.config.isDropNonPlayerKillHeads()) {
-                        this.dropStack(skull);
-                    }
-                });
+            // case: player killed by non-player
+            if (CustomPlayerHeads.config.isDropNonPlayerKillHeads()) {
+                this.dropStack(skull);
             }
         }
     }
